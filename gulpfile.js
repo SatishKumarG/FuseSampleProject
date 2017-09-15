d90a3bcb6a55591dd7aeb557dd503e1574d94236
SatishKumarG <g.satish1218@gmail.com>
2017-09-15 17:31:42 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-/** satish 
+/** satish Pradeep
  *  Welcome to your gulpfile!
  *  The gulp tasks are splitted in several files in the gulp directory
  *  because putting all here was really too long
